a15e98ae5b3cc4a2e55a3bbb3f3c378b528311ea
Jan Niestadt <jan.niestadt@ivdnt.org>
2025-02-25 09:17:55 +0100

    Add GTB response.

---
+++ fakeResponses.js
@@ -0,0 +1,404 @@
+const ANW_RESPONSE = `<?xml version="1.0" encoding="UTF-8" ?>
+<wdbs>
+
+    <totaal_aantal_artikelen>8</totaal_aantal_artikelen>
+    <totaal_aantal_items>12</totaal_aantal_items>
+
+	<wdb>
+        <gebruikersquery>koe</gebruikersquery>
+        <aangepaste_query>koe</aangepaste_query>
+        <gezocht_bij_benadering>nee</gezocht_bij_benadering>
+		<wdb_naam>anw</wdb_naam>
+		<periode>1970-heden</periode>
+        <aantal_artikelen>8</aantal_artikelen>
+        <aantal_items>12</aantal_items>
+        <artikelen>
+					<artikel>
+				<url>https://anw.ivdnt.org/article/koe</url>
+				<modern_lemma>koe</modern_lemma>
+				<historisch_lemma>koe</historisch_lemma>
+        		<woordsoort>substantief</woordsoort>
+				<betekenissen>
+										<betekenis>
+						<url>https://anw.ivdnt.org/article/koe#el:bet1.0</url>
+						<niveau>0</niveau>
+						<betekenisnummer>1.0</betekenisnummer>
+						<definitie>volwassen vrouwelijk rund</definitie>
+					</betekenis>
+										<betekenis>
+						<url>https://anw.ivdnt.org/article/koe#el:bet2.0</url>
+						<niveau>0</niveau>
+						<betekenisnummer>2.0</betekenisnummer>
+						<definitie>wijfje van een ander dier</definitie>
+					</betekenis>
+										<betekenis>
+						<url>https://anw.ivdnt.org/article/koe#el:bet3.0</url>
+						<niveau>0</niveau>
+						<betekenisnummer>3.0</betekenisnummer>
+						<definitie>vrouw</definitie>
+					</betekenis>
+										<betekenis>
+						<url>https://anw.ivdnt.org/article/koe#el:bet4.0</url>
+						<niveau>0</niveau>
+						<betekenisnummer>4.0</betekenisnummer>
+						<definitie>iets heel groots</definitie>
+					</betekenis>
+									</betekenissen>
+			</artikel>
+					<artikel>
+				<url>https://anw.ivdnt.org/article/heilige koe</url>
+				<modern_lemma>heilige koe</modern_lemma>
+				<historisch_lemma>heilige koe</historisch_lemma>
+        		<woordsoort>verbinding</woordsoort>
+				<betekenissen>
+										<betekenis>
+						<url>https://anw.ivdnt.org/article/heilige koe#el:bet1.0</url>
+						<niveau>0</niveau>
+						<betekenisnummer>1.0</betekenisnummer>
+						<definitie>koe als heilig dier</definitie>
+					</betekenis>
+										<betekenis>
+						<url>https://anw.ivdnt.org/article/heilige koe#el:bet2.0</url>
+						<niveau>0</niveau>
+						<betekenisnummer>2.0</betekenisnummer>
+						<definitie>iets met een hoge status</definitie>
+					</betekenis>
+									</betekenissen>
+			</artikel>
+					<artikel>
+				<url>https://anw.ivdnt.org/article/als een verkouden koe</url>
+				<modern_lemma>als een verkouden koe</modern_lemma>
+				<historisch_lemma>als een verkouden koe</historisch_lemma>
+        		<woordsoort>verbinding</woordsoort>
+				<betekenissen>
+										<betekenis>
+						<url>https://anw.ivdnt.org/article/als een verkouden koe#el:bet1.0</url>
+						<niveau>0</niveau>
+						<betekenisnummer>1.0</betekenisnummer>
+						<definitie>bij verkoudheid horend geluid makend</definitie>
+					</betekenis>
+									</betekenissen>
+			</artikel>
+					<artikel>
+				<url>https://anw.ivdnt.org/article/de koe bij de hoorns vatten</url>
+				<modern_lemma>de koe bij de hoorns vatten</modern_lemma>
+				<historisch_lemma>de koe bij de hoorns vatten</historisch_lemma>
+        		<woordsoort>verbinding</woordsoort>
+				<betekenissen>
+										<betekenis>
+						<url>https://anw.ivdnt.org/article/de koe bij de hoorns vatten#el:bet1.0</url>
+						<niveau>0</niveau>
+						<betekenisnummer>1.0</betekenisnummer>
+						<definitie>direct over een moeilijk onderwerp beginnen</definitie>
+					</betekenis>
+									</betekenissen>
+			</artikel>
+					<artikel>
+				<url>https://anw.ivdnt.org/article/een waarheid als een koe</url>
+				<modern_lemma>een waarheid als een koe</modern_lemma>
+				<historisch_lemma>een waarheid als een koe</historisch_lemma>
+        		<woordsoort>verbinding</woordsoort>
+				<betekenissen>
+										<betekenis>
+						<url>https://anw.ivdnt.org/article/een waarheid als een koe#el:bet1.0</url>
+						<niveau>0</niveau>
+						<betekenisnummer>1.0</betekenisnummer>
+						<definitie>een onomstotelijke waarheid</definitie>
+					</betekenis>
+									</betekenissen>
+			</artikel>
+					<artikel>
+				<url>https://anw.ivdnt.org/article/geen koe zo bont of er is wel een vlekje aan</url>
+				<modern_lemma>geen koe zo bont of er is wel een vlekje aan</modern_lemma>
+				<historisch_lemma>geen koe zo bont of er is wel een vlekje aan</historisch_lemma>
+        		<woordsoort>spreekwoord</woordsoort>
+				<betekenissen>
+										<betekenis>
+						<url>https://anw.ivdnt.org/article/geen koe zo bont of er is wel een vlekje aan#el:bet1.0</url>
+						<niveau>0</niveau>
+						<betekenisnummer>1.0</betekenisnummer>
+						<definitie>een gerucht of een roddel heeft altijd wel een kern van waarheid in zich</definitie>
+					</betekenis>
+									</betekenissen>
+			</artikel>
+					<artikel>
+				<url>https://anw.ivdnt.org/article/je weet nooit hoe een koe een haas vangt</url>
+				<modern_lemma>je weet nooit hoe een koe een haas vangt</modern_lemma>
+				<historisch_lemma>je weet nooit hoe een koe een haas vangt</historisch_lemma>
+        		<woordsoort>spreekwoord</woordsoort>
+				<betekenissen>
+										<betekenis>
+						<url>https://anw.ivdnt.org/article/je weet nooit hoe een koe een haas vangt#el:bet1.0</url>
+						<niveau>0</niveau>
+						<betekenisnummer>1.0</betekenisnummer>
+						<definitie>je weet nooit of er niet iets onverwachts of onwaarschijnlijks gebeurt</definitie>
+					</betekenis>
+									</betekenissen>
+			</artikel>
+					<artikel>
+				<url>https://anw.ivdnt.org/article/kijken als een uil op een zieke koe</url>
+				<modern_lemma>kijken als een uil op een zieke koe</modern_lemma>
+				<historisch_lemma>kijken als een uil op een zieke koe</historisch_lemma>
+        		<woordsoort>verbinding</woordsoort>
+				<betekenissen>
+										<betekenis>
+						<url>https://anw.ivdnt.org/article/kijken als een uil op een zieke koe#el:bet1.0</url>
+						<niveau>0</niveau>
+						<betekenisnummer>1.0</betekenisnummer>
+						<definitie>verbaasd, beduusd of dommig kijken</definitie>
+					</betekenis>
+									</betekenissen>
+			</artikel>
+				</artikelen>
+	</wdb>
+</wdbs>
+`;
+
+const GTB_RESPONSE = `<?xml version="1.0" encoding="UTF-8" ?>
+<wdbs>
+	<gebruikersquery>koe</gebruikersquery>
+	<aangepaste_query></aangepaste_query>
+			<gezocht_bij_benadering>nee</gezocht_bij_benadering >
+		<totaal_aantal_artikelen>11</totaal_aantal_artikelen>
+	<totaal_aantal_items>11</totaal_aantal_items>	
+	    <wdb>
+		<wdb_naam>MNW</wdb_naam>
+		<periode>1250-1550</periode>
+		<aantal_artikelen>3</aantal_artikelen>
+		<aantal_items>3</aantal_items>
+		<artikelen>
+					<artikel>
+				<url>http://gtb.inl.nl/iWDB/search?actie=article&amp;wdb=MNW&amp;id=22377&amp;lemmodern=koe</url>
+				<modern_lemma>koe</modern_lemma>
+				<historisch_lemma>COE</historisch_lemma>
+								<woordsoort>znw(v.)</woordsoort>
+				<betekenissen>
+									<betekenis>
+						<url>http://gtb.inl.nl/iWDB/search?actie=article&amp;wdb=MNW&amp;id=22377&amp;lemmodern=koe&amp;Betekenis_id=22377.sense.1</url>
+						<niveau>0</niveau>
+						<betekenisnummer></betekenisnummer>
+													<definitie>Koe.&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;</definitie>
+											</betekenis>
+									<betekenis>
+						<url>http://gtb.inl.nl/iWDB/search?actie=article&amp;wdb=MNW&amp;id=22377&amp;lemmodern=koe&amp;Betekenis_id=22377.sense.2</url>
+						<niveau>1</niveau>
+						<betekenisnummer></betekenisnummer>
+													<definitie>Met coeyen lopen, &lt;i&gt;koedrijver zijn of worden&lt;/i&gt;.&#160;&#160;</definitie>
+											</betekenis>
+								</betekenissen>
+			</artikel>
+					<artikel>
+				<url>http://gtb.inl.nl/iWDB/search?actie=article&amp;wdb=MNW&amp;id=22438&amp;lemmodern=koe</url>
+				<modern_lemma>koe</modern_lemma>
+				<historisch_lemma>COEN</historisch_lemma>
+									<homoniemnummer>I</homoniemnummer>
+								<woordsoort></woordsoort>
+				<betekenissen>
+								</betekenissen>
+			</artikel>
+					<artikel>
+				<url>http://gtb.inl.nl/iWDB/search?actie=article&amp;wdb=MNW&amp;id=23610&amp;lemmodern=koe</url>
+				<modern_lemma>koe</modern_lemma>
+				<historisch_lemma>COUWE</historisch_lemma>
+									<homoniemnummer>IV</homoniemnummer>
+								<woordsoort></woordsoort>
+				<betekenissen>
+									<betekenis>
+						<url>http://gtb.inl.nl/iWDB/search?actie=article&amp;wdb=MNW&amp;id=23610&amp;lemmodern=koe&amp;Betekenis_id=23610.sense.1</url>
+						<niveau>0</niveau>
+						<betekenisnummer></betekenisnummer>
+													<definitie>Koe.&#160;</definitie>
+											</betekenis>
+								</betekenissen>
+			</artikel>
+				</artikelen>
+	</wdb>
+	    <wdb>
+		<wdb_naam>VMNW</wdb_naam>
+		<periode>1200-1300</periode>
+		<aantal_artikelen>1</aantal_artikelen>
+		<aantal_items>1</aantal_items>
+		<artikelen>
+					<artikel>
+				<url>http://gtb.inl.nl/iWDB/search?actie=article&amp;wdb=VMNW&amp;id=ID98082&amp;lemmodern=koe</url>
+				<modern_lemma>koe</modern_lemma>
+				<historisch_lemma>COE</historisch_lemma>
+								<woordsoort>znw.v.</woordsoort>
+				<betekenissen>
+									<betekenis>
+						<url>http://gtb.inl.nl/iWDB/search?actie=article&amp;wdb=VMNW&amp;id=ID98082&amp;lemmodern=koe&amp;Betekenis_id=A98082</url>
+						<niveau>0</niveau>
+						<betekenisnummer></betekenisnummer>
+													<definitie>&#160;koe; beeld van een koe; Koe</definitie>
+											</betekenis>
+									<betekenis>
+						<url>http://gtb.inl.nl/iWDB/search?actie=article&amp;wdb=VMNW&amp;id=ID98082&amp;lemmodern=koe&amp;Betekenis_id=C98082.I.1</url>
+						<niveau>0</niveau>
+						<betekenisnummer>1</betekenisnummer>
+													<definitie>&#160;Koe, vrouwelijk huisrund. &#160;Diernaam&#160;&#160;&#160;&#160;</definitie>
+											</betekenis>
+								</betekenissen>
+			</artikel>
+				</artikelen>
+	</wdb>
+	    <wdb>
+		<wdb_naam>ONW</wdb_naam>
+		<periode>500-1200</periode>
+		<aantal_artikelen>1</aantal_artikelen>
+		<aantal_items>1</aantal_items>
+		<artikelen>
+					<artikel>
+				<url>http://gtb.inl.nl/iWDB/search?actie=article&amp;wdb=ONW&amp;id=ID2743&amp;lemmodern=koe</url>
+				<modern_lemma>koe</modern_lemma>
+				<historisch_lemma>kuo</historisch_lemma>
+								<woordsoort>znw. &#160;v.</woordsoort>
+				<betekenissen>
+									<betekenis>
+						<url>http://gtb.inl.nl/iWDB/search?actie=article&amp;wdb=ONW&amp;id=ID2743&amp;lemmodern=koe&amp;Betekenis_id=ID2743.sense.1</url>
+						<niveau>0</niveau>
+						<betekenisnummer>1</betekenisnummer>
+													<definitie>Koe, vrouwelijk rund.</definitie>
+											</betekenis>
+								</betekenissen>
+			</artikel>
+				</artikelen>
+	</wdb>
+	    <wdb>
+		<wdb_naam>WNT</wdb_naam>
+		<periode>1500-heden</periode>
+		<aantal_artikelen>6</aantal_artikelen>
+		<aantal_items>6</aantal_items>
+		<artikelen>
+					<artikel>
+				<url>http://gtb.inl.nl/iWDB/search?actie=article&amp;wdb=WNT&amp;id=M034432&amp;lemmodern=koe</url>
+				<modern_lemma>koe</modern_lemma>
+				<historisch_lemma>KOE</historisch_lemma>
+									<homoniemnummer>I</homoniemnummer>
+								<woordsoort>znw.(v.)</woordsoort>
+				<betekenissen>
+									<betekenis>
+						<url>http://gtb.inl.nl/iWDB/search?actie=article&amp;wdb=WNT&amp;id=M034432&amp;lemmodern=koe&amp;Betekenis_id=M034432.bet.1</url>
+						<niveau>0</niveau>
+						<betekenisnummer>I</betekenisnummer>
+													<definitie>Vrouwelijk rund.&#160;&#160;</definitie>
+											</betekenis>
+									<betekenis>
+						<url>http://gtb.inl.nl/iWDB/search?actie=article&amp;wdb=WNT&amp;id=M034432&amp;lemmodern=koe&amp;Betekenis_id=M034432.bet.2</url>
+						<niveau>1</niveau>
+						<betekenisnummer>I.1</betekenisnummer>
+													<definitie>Vrouwelijk huisrund, &lt;I&gt;Bos taurus domesticus&lt;/I&gt;, in den regel een dat reeds gekalfd heeft.&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;Zoo lui als een koe (V. WINSCHOOTEN, &lt;I&gt;Seeman&lt;/I&gt; 113 [1681]), &#8212; als een vogeltje dat koe heet. &#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;</definitie>
+											</betekenis>
+									<betekenis>
+						<url>http://gtb.inl.nl/iWDB/search?actie=article&amp;wdb=WNT&amp;id=M034432&amp;lemmodern=koe&amp;Betekenis_id=M034432.bet.57</url>
+						<niveau>1</niveau>
+						<betekenisnummer>I.2</betekenisnummer>
+													<definitie>Vrouwelijk exemplaar van andere soorten van het rundergeslacht, b.v. de wisents, de Amerikaansche bisons, de buffels, enz.</definitie>
+											</betekenis>
+									<betekenis>
+						<url>http://gtb.inl.nl/iWDB/search?actie=article&amp;wdb=WNT&amp;id=M034432&amp;lemmodern=koe&amp;Betekenis_id=M034432.bet.58</url>
+						<niveau>0</niveau>
+						<betekenisnummer>II</betekenisnummer>
+													<definitie>Bij uitbreiding ook gebruikt voor de wijfjes van sommige andere groote zoogdieren; b.v. in jagerstaal voor het wijfjeshert; voorts ook &#8212; gelijk in meer germ. talen &#8212; voor den wijfjeswalvisch.</definitie>
+											</betekenis>
+								</betekenissen>
+			</artikel>
+					<artikel>
+				<url>http://gtb.inl.nl/iWDB/search?actie=article&amp;wdb=WNT&amp;id=M034432.re.85&amp;lemmodern=koe~drijven </url>
+				<modern_lemma>koe~drijven </modern_lemma>
+				<historisch_lemma>Koeiendrijven</historisch_lemma>
+								<woordsoort></woordsoort>
+				<betekenissen>
+									<betekenis>
+						<url>http://gtb.inl.nl/iWDB/search?actie=article&amp;wdb=WNT&amp;id=M034432.re.85&amp;lemmodern=koe~drijven </url>
+						<niveau>0</niveau>
+						<betekenisnummer></betekenisnummer>
+													<definitie></definitie>
+											</betekenis>
+								</betekenissen>
+			</artikel>
+					<artikel>
+				<url>http://gtb.inl.nl/iWDB/search?actie=article&amp;wdb=WNT&amp;id=M034432.re.177&amp;lemmodern=koe~melken </url>
+				<modern_lemma>koe~melken </modern_lemma>
+				<historisch_lemma>Koemelken</historisch_lemma>
+								<woordsoort></woordsoort>
+				<betekenissen>
+									<betekenis>
+						<url>http://gtb.inl.nl/iWDB/search?actie=article&amp;wdb=WNT&amp;id=M034432.re.177&amp;lemmodern=koe~melken </url>
+						<niveau>0</niveau>
+						<betekenisnummer></betekenisnummer>
+													<definitie></definitie>
+											</betekenis>
+								</betekenissen>
+			</artikel>
+					<artikel>
+				<url>http://gtb.inl.nl/iWDB/search?actie=article&amp;wdb=WNT&amp;id=M034433&amp;lemmodern=koe</url>
+				<modern_lemma>koe</modern_lemma>
+				<historisch_lemma>KOE</historisch_lemma>
+									<homoniemnummer>II</homoniemnummer>
+								<woordsoort>znw.</woordsoort>
+				<betekenissen>
+									<betekenis>
+						<url>http://gtb.inl.nl/iWDB/search?actie=article&amp;wdb=WNT&amp;id=M034433&amp;lemmodern=koe&amp;Betekenis_id=M034433.bet.1</url>
+						<niveau>0</niveau>
+						<betekenisnummer></betekenisnummer>
+													<definitie>Lederen handbekleedsel, door metselaars en steenkruiers gebruikt om het stukschuren der handen door de gedurige aanraking met de ruwe en dikwijls nog heete steenen te voorkomen (ZWIERS [1918]). Misschien is &lt;I&gt;koeien&lt;/I&gt; eigenlijk de aanduiding van de stof (koeleder) waarvan deze bekleedsels gemaakt worden.</definitie>
+											</betekenis>
+								</betekenissen>
+			</artikel>
+					<artikel>
+				<url>http://gtb.inl.nl/iWDB/search?actie=article&amp;wdb=WNT&amp;id=M034434&amp;lemmodern=koe</url>
+				<modern_lemma>koe</modern_lemma>
+				<historisch_lemma>KOE</historisch_lemma>
+									<homoniemnummer>III</homoniemnummer>
+								<woordsoort>znw.</woordsoort>
+				<betekenissen>
+									<betekenis>
+						<url>http://gtb.inl.nl/iWDB/search?actie=article&amp;wdb=WNT&amp;id=M034434&amp;lemmodern=koe&amp;Betekenis_id=M034434.bet.1</url>
+						<niveau>0</niveau>
+						<betekenisnummer>1</betekenisnummer>
+													<definitie>Kinkhoren (&lt;I&gt;Loquela (Wdb.)&lt;/I&gt; [Klemskerke, 1907]).&#160;</definitie>
+											</betekenis>
+									<betekenis>
+						<url>http://gtb.inl.nl/iWDB/search?actie=article&amp;wdb=WNT&amp;id=M034434&amp;lemmodern=koe&amp;Betekenis_id=M034434.bet.2</url>
+						<niveau>0</niveau>
+						<betekenisnummer>2</betekenisnummer>
+													<definitie>Benaming voor het geluid waarmee men iemand in de verte roept; in de Kempen (CORN.-VERVL.); bij TUERL.&#160;&lt;I&gt;koehoe.&lt;/I&gt; Misschien oorspr. gebezigd om het dier &#8221;koe&#8221; te roepen en dan het woord &lt;I&gt;Koe&lt;/I&gt; (I): verg. in dezen zin &lt;I&gt;koi&lt;/I&gt; aan de Zaan (BOEKENOOGEN 1333).&#160;</definitie>
+											</betekenis>
+									<betekenis>
+						<url>http://gtb.inl.nl/iWDB/search?actie=article&amp;wdb=WNT&amp;id=M034434&amp;lemmodern=koe&amp;Betekenis_id=M034434.bet.3</url>
+						<niveau>0</niveau>
+						<betekenisnummer>3</betekenisnummer>
+													<definitie>In Z.-Nederl. volgens SCHUERM. [1865-1870]: oprisping.&#160;</definitie>
+											</betekenis>
+								</betekenissen>
+			</artikel>
+					<artikel>
+				<url>http://gtb.inl.nl/iWDB/search?actie=article&amp;wdb=WNT&amp;id=M034435&amp;lemmodern=koe</url>
+				<modern_lemma>koe</modern_lemma>
+				<historisch_lemma>KOE</historisch_lemma>
+									<homoniemnummer>IV</homoniemnummer>
+								<woordsoort>znw.</woordsoort>
+				<betekenissen>
+									<betekenis>
+						<url>http://gtb.inl.nl/iWDB/search?actie=article&amp;wdb=WNT&amp;id=M034435&amp;lemmodern=koe&amp;Betekenis_id=M034435.bet.1</url>
+						<niveau>0</niveau>
+						<betekenisnummer>1</betekenisnummer>
+													<definitie>In West-Friesl. gebezigd bij het kaartspel als iemand zijn slag haalt. &#160;</definitie>
+											</betekenis>
+									<betekenis>
+						<url>http://gtb.inl.nl/iWDB/search?actie=article&amp;wdb=WNT&amp;id=M034435&amp;lemmodern=koe&amp;Betekenis_id=M034435.bet.2</url>
+						<niveau>0</niveau>
+						<betekenisnummer>2</betekenisnummer>
+													<definitie>Op de Veluwe: dubbel vijf in &apos;t dominospel (&lt;I&gt;Nav.&lt;/I&gt; 30, 358).&#160;</definitie>
+											</betekenis>
+									<betekenis>
+						<url>http://gtb.inl.nl/iWDB/search?actie=article&amp;wdb=WNT&amp;id=M034435&amp;lemmodern=koe&amp;Betekenis_id=M034435.bet.3</url>
+						<niveau>0</niveau>
+						<betekenisnummer>3</betekenisnummer>
+													<definitie>In de verbinding op de koe staan, bijna verloren zijn bij het spel (V. DALE&#7459;). Wellicht is &lt;I&gt;koe&lt;/I&gt; hier &lt;I&gt;coup de gr&#226;ce&lt;/I&gt; (HARREB. 1, 425 &lt;I&gt;a&lt;/I&gt; [1858]); voor de grap teekent men bij den naam van den speler die op de koe staat een koe (dier).&#160;</definitie>
+											</betekenis>
+								</betekenissen>
+			</artikel>
+				</artikelen>
+	</wdb>
+	</wdbs>`;
