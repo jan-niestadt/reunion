0338ca7b72dc008bd12b1f360ae9858fbb4c5213
Jan Niestadt <jan.niestadt@ivdnt.org>
2025-03-02 10:18:47 +0100

    HTML builder, but can't nest elements easily...

--- services/anw.js
+++ services/anw.js
@@ -1,5 +1,6 @@
 import *  as XML from '../lib/xml.js';
 import { unifyPartOfSpeech } from '../lib/util.js';
+import { qa } from '../lib/reunion.js';
 
 export default {
 	// The service we're querying
@@ -27,10 +28,87 @@ export default {
 				const results = [];
 				const arts = XML.findSingleElement(data, 'artikelen');
 				
-				const { link, linkIcon, sentence, listItem, i } = reporter.htmlBuilder;
+				//const { link, linkIcon, sentence, listItem, i } = reporter.htmlBuilder;
+				const htmlBuilder = () => {
+					const parts = [];
+
+					const builder = {
+						toString() {
+							return parts.join('');
+						},
+						html(html) {
+							parts.push(html);
+							return this;
+						},
+						text(text) {
+							return this.html(qa(text));
+						},
+						sentence(text) {
+							return this.text(text.replace(/(\p{L})\s*$/gu, '$1.'));
+						},
+						htmlSentence(html) {
+							return this.html(html.replace(/(\p{L})\s*$/gu, '$1.'));
+						},
+						listItem(html, ordinal) {
+							if (!ordinal)
+								ordinal = '▪';
+							else
+								ordinal = ordinal.replace(/([\d\w])$/, '$1.');
+							return this.html(`<div class='list-item'><span class='ordinal'>${ordinal}</span><span class='content'>${html}</span></div>`);
+						},
+						link(html, url, className) {
+							if (Array.isArray(className))
+								className = className.join(' ');
+							const optClass = className ? ` class="${qa(className)}"` : '';
+							return this.html(`<a target="_blank" href="${qa(url, true)}"${optClass}>${qa(isEmpty(html) ? url : html)}</a>`);
+						},
+						linkIcon(url, className) {
+							if (Array.isArray(className))
+								className = className.join(' ');
+							className += ' icon';
+							return this.link('📘', url, className);
+						},
+						moreLink(url, className) {
+							if (Array.isArray(className))
+								className = className.join(' ');
+							className += ' more';
+							return this.link('meer »', url, className);
+						},
+						el(elName, innerHtml, className, evenIfEmpty = false) {
+							if (!evenIfEmpty && isEmpty(innerHtml))
+								return '';
+							if (Array.isArray(className))
+								className = className.join(' ');
+							const optClass = className
+								? ` class="${qa(className)}"`
+								: '';
+							return this.html(`<${elName}${optClass}>${innerHtml}</${elName}>`);
+						},
+						span(html, className) {
+							return this.el('span', html, className);
+						},
+						div(html, className) {
+							return this.el('div', html, className);
+						},
+						b(html) {
+							return this.el('strong', html);
+						},
+						i(html) {
+							return this.el('em', html);
+						}
+					
+					};
+					return builder;
+				};
 
 				XML.forEachChildElement(arts, art => {
 					if (art.nodeType === Element.ELEMENT_NODE) {
+						const html = htmlBuilder();
+						const lemma = XML.getElementValue(art, 'modern_lemma');
+						const url = XML.getElementValue(art, 'url');
+						const woordsoort = unifyPartOfSpeech(XML.getElementValue(art, 'woordsoort'));
+						html.link(lemma, url).text(' ').
+
 						const snippet = [];
 						const bets = XML.findSingleElement(art, 'betekenissen');
 						XML.forEachChildElement(bets, bet => {
@@ -40,9 +118,6 @@ export default {
 							const content = `${sentence(definitie)} ${linkIcon(url)}`;
 							snippet.push(listItem(content, nr === '0.0' ? '' : nr));
 						});
-						const lemma = XML.getElementValue(art, 'modern_lemma');
-						const url = XML.getElementValue(art, 'url');
-						const woordsoort = unifyPartOfSpeech(XML.getElementValue(art, 'woordsoort'));
 						results.push({
 							main: `${link(lemma, url)} ${i(woordsoort)}`,
 							snippet
